c96bd6029275bd55c4db7bab843cd56d88d3b533
AritriBasu <AritriBasu@users.noreply.github.com>
2021-05-25 02:43:40 +0530

    added account route

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
+let type="";
 router.get('/', function(req, res){
     console.log(req.session.email);
     console.log(req.session.type);
+    type=req.session.type;
     res.render('index');
 });
 
@@ -40,8 +41,24 @@ router.get("/signup/signup_intern", function(req,res){
     res.render("signup_intern");
 });
 
-router.get("/account/account_investor", function(req,res){
-    res.render("Account");
+router.get("/account", function(req,res){
+    if(type==="investor"){
+        try {
+            console.log(req.session.email);
+            res.render('account_investor', {
+              email: req.session.email
+            })
+          } catch (err) {
+            console.error(err);
+          }
+    }
+    else if(type==="intern")
+    {
+        res.send("intern route");
+    }
+    else{
+        res.send("startup route")
+    }
 });
 
 module.exports = router;
