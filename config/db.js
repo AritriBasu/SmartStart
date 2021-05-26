61161a8e6bfea2af865e149fa8c177a6c5acab86
B-u-f-f <kshitij957@gmail.com>
2021-05-27 04:01:32 +0530

    done

--- config/db.js
+++ config/db.js
@@ -2,15 +2,9 @@ const mysql = require('mysql')
 
 const con = mysql.createConnection({
     host: "localhost",
-<<<<<<< HEAD
-    user: "root",
-    password: "",
-    database: 'IWPDB1',
-=======
     user: "god",
     password: "god",
     database: 'IWPDB',
->>>>>>> completed
     multipleStatements: true
 });
 
